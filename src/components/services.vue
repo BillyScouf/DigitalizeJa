5e312516a5b347a0445d93908025bf86c382fa82
Andersio Lopes <andersiolopes2804@gmail.com>
2025-11-22 15:50:48 +0100

    add new branch

--- src/components/services.vue
+++ src/components/services.vue
@@ -1,11 +1,12 @@
 <!-- src/components/Services.vue -->
 <template>
 <section id="servicos" class="services">
-  <div class="card">
-    <h3>Criação de Sites Estáticos</h3>
-    <p>Desenvolvimento de sites rápidos, modernos e totalmente responsivos para pequenas e médias empresas.</p>
-    <a href="#">Saiba mais →</a>
-  </div>
+    <div class="card">
+        <h3>Criação de Sites Estáticos</h3>
+        <p>Desenvolvimento de sites rápidos, modernos e totalmente responsivos para pequenas e médias empresas.</p>
+        <p class="price">Preço: 50–100 kz</p>
+        <a href="#">Saiba mais →</a>
+        </div>
 
   <div class="card">
     <h3>Landing Pages Profissionais</h3>
@@ -43,6 +44,12 @@
   padding: 2rem;
   max-width: 1100px;
   margin: auto;
+}
+
+.price {
+  font-weight: bold;
+  color: #ff6600; /* cor chamativa */
+  margin-top: 10px;
 }
 
 /* Card */
